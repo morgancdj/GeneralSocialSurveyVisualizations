7ee06d33464806f2191c10c846ea2623c9c63054
Morgan Jackson <morganjackson@morgans-mbp-2.mynetworksettings.com>
2024-12-08 10:19:36 -0500

    Fixed? some problems in results

--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -1,1 +1,40 @@
 // add your JavaScript/D3 to this file
+//TODO: import data, implement slider
+//
+
+//import cut-down CSV with the data we want to show
+
+const years = [];
+const values = [];
+
+d3.csv("dummy_data.csv", function(data){
+  for (var i = 0; i &lt; data.length; i++){
+    years.push(data[i].year);
+    console.log(data[i].year);
+    values.push(data[i].value);
+    console.log(data[i].year);
+  }
+});
+
+//slider with years from d3-simple-slider on github, cite this as source
+
+var slider = d3
+  .sliderHorizontal()
+  .min(years[0])
+  .max(years[years.length - 1])
+  .step(1)
+  .width(300)
+  .displayValue(false)
+  .on('onchange', (val) => {
+    d3.select('#value').text(val)
+});
+
+d3.select('#slider')
+  .append('svg')
+  .attr('width', 500)
+  .attr('height', 100)
+  .append('g')
+  .attr('transform', 'translate(30,30)')
+  .call(slider);
+
+//moving slider changes lookup variable by years, allows user to slide backwards and fowards in time to see trends
